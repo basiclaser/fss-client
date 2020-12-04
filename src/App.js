a443a19c5e80daab795b7303ea7b337331fc262d
Christopher Shelley <dont@forgetmy.email>
2020-12-04 17:56:23 +0100

    init repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const formRef = useRef();
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:4000")
+        .then((res) => res.json())
+        .then((res) => setMessages(res));
+    }, 10000);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      from: formRef.current["from"].value,
+      message: formRef.current["message"].value,
+    };
+    fetch("http://localhost:4000", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setMessages((prev) => [...prev, res]);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <input name="from" type="text" placeholder="from"></input>
+        <input name="message" type="text" placeholder="message"></input>
+        <input type="submit" />
+      </form>
+      <ul>
+        {messages.map((m) => (
+          <li>
+            ({m.from}) - {m.message}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
